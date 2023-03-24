55e5e35fc902aa582d5c36d529cbdc7c3af7c2fc
Alexander Tretyakov <tpetyakov@gmail.com>
2023-03-24 21:38:09 +0300

    feat: add routes test

--- jest.config.ts
+++ jest.config.ts
@@ -4,20 +4,23 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/__mocks__/**'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
+    '\\.(scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@common/(.*)$': '<rootDir>/src/common/$1',
     '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    '\\.(scss)$': 'identity-obj-proxy',
   },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
 
 export default config;
---
+++ src/components/App/index.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '.';
+
+describe('<App /> test', () => {
+  test('Test for good route', () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('link', { name: /about/i })).toHaveClass('active');
+  });
+
+  test('Test for bad route', () => {
+    render(
+      <MemoryRouter initialEntries={['/bad-route']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('heading', { name: /404/i })).toBeInTheDocument();
+  });
+});
