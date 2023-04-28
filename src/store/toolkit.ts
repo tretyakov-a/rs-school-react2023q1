066dbf309459a7cb2064aeaf87086d963bb36d2b
Alexander Tretyakov <tpetyakov@gmail.com>
2023-04-28 12:12:37 +0300

    refactor: get rid off eslint-disable

--- src/store/toolkit.ts
+++ src/store/toolkit.ts
@@ -1,5 +1,6 @@
 import * as toolkitRaw from '@reduxjs/toolkit';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const { configureStore, createAsyncThunk, createSlice } = ((toolkitRaw as any).default ??
-  toolkitRaw) as typeof toolkitRaw;
+type ToolkitRaw = typeof toolkitRaw & { default?: unknown };
+
+export const { configureStore, createAsyncThunk, createSlice } = ((toolkitRaw as ToolkitRaw)
+  .default ?? toolkitRaw) as typeof toolkitRaw;
